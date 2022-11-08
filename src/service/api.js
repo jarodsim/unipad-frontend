0b25f9156193b188a4b9bfe9f774c57e1936c2f5
Jarod Cavalcante <jarodsim@gmail.com>
2022-11-08 18:57:16 -0300

    update: atualizando libs e link da API backend

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://unipadback.herokuapp.com/',
+  baseURL: 'https://unipadbackend.jarodmateus.com/',
 })
 
 export default api
