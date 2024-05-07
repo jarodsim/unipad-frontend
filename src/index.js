5b95bc7bdfa51f76b2d8911e8a43d3d6a38aded6
Jarod Cavalcante <jarodsim@gmail.com>
2024-05-07 13:50:04 -0300

    feat: Add React GA tracking and initialize on page load

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { ThemeProvider } from '@mui/material'
+import { ThemeProvider } from '@mui/material';
 
-import GlobalStyles, { theme } from './globalStyles'
-import Routes from './routes'
-import reportWebVitals from './reportWebVitals'
-import Header from './components/Header'
-import { PadProvider } from './context/padContext'
-import { MenuProvider } from './context/menuContext'
-import { AuthProvider } from './context/authContext'
-import { SnackbarProvider } from './context/snackbarContext'
-import { LoadingProvider } from './context/loadingContext'
+import GlobalStyles, { theme } from './globalStyles';
+import Routes from './routes';
+import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import { PadProvider } from './context/padContext';
+import { MenuProvider } from './context/menuContext';
+import { AuthProvider } from './context/authContext';
+import { SnackbarProvider } from './context/snackbarContext';
+import { LoadingProvider } from './context/loadingContext';
+
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = 'G-Z33D84EBN9';
+
+ReactGA.initialize(TRACKING_ID);
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+  title: document.title,
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -36,9 +47,9 @@ ReactDOM.render(
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
