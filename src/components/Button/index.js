050f047f867b23f21cb283b5c501ea4e6ef465f8
Jarod Cavalcante <jarodsim@gmail.com>
2026-03-29 18:49:03 -0300

    atualizando libs

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { MyButton } from './styles'
 
 export default function Button({ label, color, callback }) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { ThemeProvider } from '@mui/material';
@@ -14,7 +14,10 @@ import { AuthProvider } from './context/authContext';
 import { SnackbarProvider } from './context/snackbarContext';
 import { LoadingProvider } from './context/loadingContext';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
@@ -34,8 +37,7 @@ ReactDOM.render(
         </SnackbarProvider>
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
